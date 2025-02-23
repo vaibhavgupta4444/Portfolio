16f3da53bdf35c81610157f9a1c6125b6ad67d74
vaibhavInnovation <vaibhav.ayushgupta@gmail.com>
2025-02-23 16:05:37 +0530

    versel.json file

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,7 @@ import {motion} from 'motion/react'
 const Project = ({src}) => {
   return (
     <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:2,ease:'easeInOut'}} className='md:w-1/2 w-full md:h-[65vh] h-1/2 border-2'>
-      <iframe src={src} loading='lazy' className='w-full h-full'/>
+      <iframe src={src} loading='lazy' sandbox="allow-scripts allow-same-origin" className='w-full h-full'/>
     </motion.div>
   )
 }
