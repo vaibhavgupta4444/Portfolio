6ecbd7e268699a790a00366ae17f5250076afb84
vaibhavInnovation <vaibhav.ayushgupta@gmail.com>
2025-02-23 15:39:58 +0530

    profile image delete

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,10 +1,9 @@
 import sun_light from './sun_light.png'
 import send from './send.png'
-import me from './ME.png'
 import resume from './resume.pdf'
 
 export const assets={
     sun_light,
-    send,me,
+    send,
     resume
 }
