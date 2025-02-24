220795493803347e56d5103572e4a29c33233a64
vaibhavInnovation <vaibhav.ayushgupta@gmail.com>
2025-02-24 22:45:12 +0530

    bot component

--- src/components/Bot.jsx
+++ src/components/Bot.jsx
@@ -6,7 +6,7 @@ export default function Bot() {
   return (
     <div className="h-full w-full bot">
       <Suspense fallback={<p className="text-white text-lg w-full h-full flex justify-center items-end">Loading 3D Model...</p>}>
-        {/* <LazySpline scene="https://prod.spline.design/g39g8PWV3LkcOiE3/scene.splinecode" /> */}
+        <LazySpline scene="https://prod.spline.design/g39g8PWV3LkcOiE3/scene.splinecode" />
       </Suspense>
     </div>
   );
